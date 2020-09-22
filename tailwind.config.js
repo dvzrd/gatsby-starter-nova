6b6a17e527cc26a91ec41fc73ff666c32f8f1554
Damir Vazgird <damir@withpulp.com>
2020-09-21 17:15:25 -0700

    added primary and secondary color vars to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,30 @@ module.exports = {
     },
   },
   theme: {
+    colors: {
+      primary: {
+        '100': 'var(--color-primary-100)',
+        '200': 'var(--color-primary-200)',
+        '300': 'var(--color-primary-300)',
+        '400': 'var(--color-primary-400)',
+        '500': 'var(--color-primary-500)',
+        '600': 'var(--color-primary-600)',
+        '700': 'var(--color-primary-700)',
+        '800': 'var(--color-primary-800)',
+        '900': 'var(--color-primary-900)',
+      },
+      secondary: {
+        '100': 'var(--color-secondary-100)',
+        '200': 'var(--color-secondary-200)',
+        '300': 'var(--color-secondary-300)',
+        '400': 'var(--color-secondary-400)',
+        '500': 'var(--color-secondary-500)',
+        '600': 'var(--color-secondary-600)',
+        '700': 'var(--color-secondary-700)',
+        '800': 'var(--color-secondary-800)',
+        '900': 'var(--color-secondary-900)',
+      },
+    },
     container: {
       center: true,
       padding: {
