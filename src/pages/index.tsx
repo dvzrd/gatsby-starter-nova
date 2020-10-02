2cd6154ad8e8c5e789f102442b63a7f7ad4acd95
Damir Vazgird <damir@withpulp.com>
2020-10-01 23:57:15 -0700

    updates to index page example sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,41 +8,49 @@ import { useSiteMetadata } from "graphql";
 interface PageProps extends GatsbyPageProps {}
 
 const PageIndex: FC<PageProps> = () => {
-  const { defaultDescription } = useSiteMetadata();
+  const { defaultDescription, defaultTitle, name } = useSiteMetadata();
 
   return (
     <DefaultLayout>
       <Section as="header" pattern="hero">
-        <h2 className="leading-tight my-8 lg:my-16 text-primary-500">
-          {defaultDescription}
-        </h2>
+        <h2 className="leading-tight my-8 lg:my-16">{defaultDescription}</h2>
+      </Section>
+      <Section className="bg-background-primary" pattern="feature">
+        <h3 className="leading-tight mt-8 mb-4 text-copy-primary">
+          {defaultTitle}
+        </h3>
+        <p className="mb-8 text-lg text-copy-primary">
+          Nulla sit amet suscipit diam. Praesent aliquam metus nec diam
+          ultricies tincidunt. Donec congue feugiat tempus. Pellentesque mi
+          lacus, fermentum sit amet lectus quis, iaculis volutpat massa.
+        </p>
       </Section>
       <Section className="bg-background-paper" pattern="content">
         <h4 className="leading-tight mt-8 mb-6 text-copy-paper">
-          Content with paper colors
+          Tailwind CSS Utility Classes
         </h4>
-        <p className="mb-6 text-copy-paper">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rhoncus
-          massa vel risus commodo, bibendum faucibus ligula commodo. Sed
-          sollicitudin nec eros sed condimentum. Suspendisse eget viverra velit,
-          ac faucibus velit. Nulla eget semper leo. Curabitur tincidunt semper
-          erat, eu finibus elit auctor sed. Sed egestas tempor interdum. Etiam
-          eu massa euismod purus dictum faucibus.
-        </p>
-        <p className="mb-8 text-copy-paper">
-          Sed at felis fermentum, rutrum eros quis, sodales leo. Etiam porta,
-          ipsum quis sagittis mollis, massa metus vulputate neque, sit amet
-          convallis turpis leo ut nulla. Morbi sed gravida lectus. Pellentesque
-          sodales tincidunt diam ut tristique. Fusce venenatis ligula at erat
-          ultrices, et mattis neque fringilla. Vivamus ligula tortor, porttitor
-          a augue non, elementum facilisis odio.
-        </p>
+        <div className="lg:gap-6 grid grid-cols-1 lg:grid-cols-2">
+          <p className="mb-6 text-copy-paper">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            rhoncus massa vel risus commodo, bibendum faucibus ligula commodo.
+            Sed sollicitudin nec eros sed condimentum. Suspendisse eget viverra
+            velit, ac faucibus velit. Nulla eget semper leo. Curabitur tincidunt
+            semper erat, eu finibus elit auctor sed. Sed egestas tempor
+            interdum. Etiam eu massa euismod purus dictum faucibus.
+          </p>
+          <p className="mb-8 text-copy-paper">
+            Sed at felis fermentum, rutrum eros quis, sodales leo. Etiam porta,
+            ipsum quis sagittis mollis, massa metus vulputate neque, sit amet
+            convallis turpis leo ut nulla. Morbi sed gravida lectus.
+            Pellentesque sodales tincidunt diam ut tristique. Fusce venenatis
+            ligula at erat ultrices, et mattis neque fringilla. Vivamus ligula
+            tortor, porttitor a augue non, elementum facilisis odio.
+          </p>
+        </div>
       </Section>
-      <Section as="footer" className="bg-background-primary" pattern="feature">
-        <h3 className="leading-tight mt-8 mb-4 text-copy-primary">
-          Feature with primary colors
-        </h3>
-        <p className="mb-8 text-lg text-copy-primary">
+      <Section as="footer">
+        <h3 className="leading-tight mt-8 mb-4">{name}</h3>
+        <p className="mb-8 text-lg">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rhoncus
           massa vel risus commodo, bibendum faucibus ligula commodo. Sed
           sollicitudin nec eros sed condimentum.
